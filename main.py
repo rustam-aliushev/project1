128ebd87dad2d9e84b61f196303ec139cc7a39bb
rustam-aliushev <alrustamdi@gmail.com>
2024-09-21 01:39:00 +0300

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print(5)
+import shiny
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pd.read_csv('weights_heights.csv', index_col='Index')
+X1 = data['Height'].tolist()
+X1 = [[x] for x in X1]
+y1 = data['Weight'].tolist()
+
+reg = LinearRegression().fit(X1, y1)
+
+y_pred = reg.predict(X1)
+plt.scatter(X1, y1, color='blue', label='Данные')
+plt.plot(X1, y_pred, color='red', label='Линия регрессии')
+plt.xlabel('X1')
+plt.ylabel('y1')
+plt.title('Линейная регрессия')
+plt.legend()
+plt.show()
